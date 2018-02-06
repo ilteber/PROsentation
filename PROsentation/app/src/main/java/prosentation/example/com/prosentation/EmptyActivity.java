c30ac6a86c4d2ffb954b85a777c750f705a4a89a
Ömer Mesud TOKER <mesudtoker@gmail.com>
2018-02-06 19:44:57 +0300

    Login ve Signup
    Login ve Signup eklendi

---
+++ PROsentation/app/src/main/java/prosentation/example/com/prosentation/EmptyActivity.java
@@ -0,0 +1,17 @@
+package prosentation.example.com.prosentation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lenovo on 6.2.2018.
+ */
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ PROsentation/app/src/main/java/prosentation/example/com/prosentation/UserMapper.java
@@ -0,0 +1,45 @@
+package prosentation.example.com.prosentation;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+/**
+ * Created by lenovo on 6.2.2018.
+ */
+
+
+@DynamoDBTable(tableName = "users")
+public class UserMapper {
+    private String username;
+    private String email;
+    private String password;
+
+    @DynamoDBHashKey(attributeName = "username")
+    @DynamoDBAttribute(attributeName = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @DynamoDBAttribute(attributeName = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @DynamoDBAttribute(attributeName = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
